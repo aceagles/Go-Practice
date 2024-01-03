8e4619b9648fcc161a38569a315bdf24080b09d3
Adam Eagles <ad.c.eagles@gmail.com>
2024-01-03 21:06:12 +0000

    modbus Desktop app using vue frontend

---
+++ cmd/modbusApp/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {onMounted, reactive} from 'vue'
+import {Greet2, GetCount} from '../../wailsjs/go/main/App'
+
+const data = reactive({
+  name: "",
+  resultText: "Please enter your name below 👇",
+  timer: "",
+  counter: 0
+})
+
+function greet() {
+  Greet2(data.name).then(result => {
+    data.resultText = result
+  })
+}
+function updateCounter() {
+  GetCount().then(result => data.counter = result)
+}
+
+onMounted(function () {
+  data.timer = setInterval(updateCounter, 500)
+  console.log("Mounted")
+})
+
+</script>
+
+<template>
+
+    
+    <p class="bg-primary">Counter: {{data.counter}}</p>
+  
+</template>
+
+
+<style scoped>
+
+</style>
