ad0195363ddde5f9e79937695b0ef2e63ce0b55b
Adam Eagles <ad.c.eagles@gmail.com>
2024-01-05 17:07:35 +0000

    Moving modbus app to its own repo

--- cmd/modbusApp/frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import './style.css';
-import './assets/css/bootstrap.min.css';
-
-// Import Bootstrap JS
-import './assets/js/bootstrap.min.js';
-
-createApp(App).mount('#app')
